2bb3572edad04d9f8774725daeee066b9ef6a45a
chengjunjie <chengjunjie.jack@gmail.com>
2023-06-14 01:25:19 +0800

    update

---
+++ mvc/src/main/java/com/example/servletlearn/mvc/filter/AuthFilter.java
@@ -0,0 +1,25 @@
+package com.example.servletlearn.mvc.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/user/*")
+public class AuthFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        if (req.getSession().getAttribute("user") == null) {
+            // 未登录，自动跳转到登录页:
+            System.out.println("AuthFilter: not login!");
+            resp.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            // 已登录，继续处理:
+            chain.doFilter(request, response);
+        }
+    }
+}
