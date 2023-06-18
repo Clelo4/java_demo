1f40978f96d23928d8768fdabeb95b53575792e2
chengjunjie <chengjunjie.jack@gmail.com>
2023-06-18 18:27:42 +0800

    update

--- mvc/src/main/java/com/example/servletlearn/mvc/bean/User.java
+++ mvc/src/main/java/com/example/servletlearn/mvc/bean/User.java
@@ -1,7 +1,4 @@
 package com.example.servletlearn.mvc.bean;
 
-public class User {
-    public String username;
-    public String email;
-    public String password;
+public record User(String username, String email, String password, String createTime) {
 }
--- mvc/src/main/java/com/example/servletlearn/mvc/controller/UserController.java
+++ mvc/src/main/java/com/example/servletlearn/mvc/controller/UserController.java
@@ -5,9 +5,10 @@ import com.example.servletlearn.mvc.framework.GetMapping;
 import com.example.servletlearn.mvc.framework.ModelAndView;
 import com.example.servletlearn.mvc.framework.PostMapping;
 import com.example.servletlearn.mvc.utils.DB;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserController {
@@ -15,17 +16,29 @@ public class UserController {
     public User index() {
         try {
             DB db = new DB();
-            db.query();
+
+            try (
+                Connection conn = db.getConn();
+                PreparedStatement stmt = conn.prepareStatement("SELECT username, email, password, create_time FROM user");
+            ) {
+                ResultSet rs = stmt.executeQuery();
+                String username = null;
+                String email = null;
+                String password = null;
+                String createTime = null;
+                while (rs.next()) {
+                    username = rs.getString(1);
+                    email = rs.getString(2);
+                    password = rs.getString(3);
+                    createTime = rs.getString(4);
+                }
+
+                return new User(username, email, password, createTime);
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-        User user = new User();
-        user.username = "jack";
-        user.password = "password";
-        user.email = "email";
-
-        return user;
     }
     @GetMapping("/user/string")
     public String string() {
@@ -36,16 +49,11 @@ public class UserController {
             throw new RuntimeException(e);
         }
 
-        User user = new User();
-        user.username = "jack";
-        user.password = "password";
-        user.email = "email";
-
         return "user";
     }
 
     @PostMapping("/addUser")
     public ModelAndView addUser(User user) {
-        return new ModelAndView("./index.html", "name", user.username);
+        return new ModelAndView("./index.html", "name", user.username());
     }
 }
